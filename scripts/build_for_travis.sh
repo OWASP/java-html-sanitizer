a1dd006133971008ec8b3a6654e54f1547d482a5
Mike Samuel <mikesamuel@gmail.com>
2019-03-20 09:54:19 -0400

    Update guava.version to 27.1
    https://github.com/OWASP/java-html-sanitizer/issues/162 identifies a
    problem with depending on older guava versions.
    
    https://ossindex.sonatype.org/vuln/24585a7f-eb6b-4d8d-a2a9-a6f16cc7c1d0
    affects Guava < 24.1.
    
    This does not directly affect the sanitizer but could allow denial of
    service of clients.
    
    Clients that require older JDKs should use `-Dguava.version=` with `mvn`
    to specify a version of their choice or add to their POM's `<properties>`
    element
    
    ```xml
      <guava.version>123.1</guava.version>
    ```

--- scripts/build_for_travis.sh
+++ scripts/build_for_travis.sh
@@ -10,10 +10,10 @@ if echo $TRAVIS_JDK_VERSION | egrep -q 'jdk[67]'; then
     # The main library only uses jdk6 incompatible dependencies,
     # and older versions of javadoc barf on -Xdoclint flags used
     # to configure the maven-javadoc-plugin.
-    exec mvn verify -Dmaven.javadoc.skip=true $COMMON_FLAGS
+    exec mvn verify -Dguava.version=20.0 -Dmaven.javadoc.skip=true $COMMON_FLAGS
 else
     # Build the whole kit-n-kaboodle.
     mvn                             -f aggregate/pom.xml       source:jar javadoc:jar verify $COMMON_FLAGS \
-    && mvn -Dguava.version=27.0-jre -f aggregate/pom.xml clean source:jar javadoc:jar verify $COMMON_FLAGS \
+    && mvn -Dguava.version=27.1-jre -f aggregate/pom.xml clean source:jar javadoc:jar verify $COMMON_FLAGS \
     && mvn jacoco:report coveralls:report
 fi
