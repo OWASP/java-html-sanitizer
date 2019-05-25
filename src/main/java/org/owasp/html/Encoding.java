e8a0a8bc3afaaba2e01f2b396d1bf88fc8d9f633
Jacob Shields <jshields@squarespace.com>
2019-05-25 14:04:06 -0400

    Update Javadoc

--- src/main/java/org/owasp/html/Encoding.java
+++ src/main/java/org/owasp/html/Encoding.java
@@ -37,7 +37,7 @@ public final class Encoding {
 
   /**
    * Decodes HTML entities to produce a string containing only valid
-   * Unicode code units.
+   * Unicode code points.
    *
    * @param s text/html
    * @return text/plain
